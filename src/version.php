2797dfd9184c46e4097d8813512d7c8286791755
Gustavo Bazzo <gcbazzo@gmail.com>
2024-05-10 15:38:24 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -27,19 +27,19 @@ defined('MOODLE_INTERNAL') || die();
  
 // This is the version of the plugin.                                                                                               
 
-$plugin->version = 2023011900;
+$plugin->version = 2024050900;
 
 // This is the named version.
-$plugin->release = 'v1.2.1-stable';  
+$plugin->release = 'v1.2.2-stable';  
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
 
 // This is the version of Moodle this plugin requires.                                                                              
-$plugin->requires = 2020061500.00; // Moodle 3.9.0                                                                                         
+$plugin->requires = 2022041900.00; // Moodle 4.0.0                                                                       
 // This is the component name of the plugin - it always starts with 'theme_'                                                        
 // for themes and should be the same as the name of the folder.                                                                     
 $plugin->component = 'theme_recitlegacy';
-$plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
+$plugin->supported = [400, 403];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [
